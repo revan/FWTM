f5212fa225862533b4db99e24f2f90ff109c03c1
Revan Sopher <rsopher@gmail.com>
2015-03-07 18:36:04 -0500

    Block.contains()

--- server/block.py
+++ server/block.py
@@ -1,11 +1,17 @@
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
 class Block:
     def __init__(self, id, boundaries):
         self.id = id
         self.boundaries = boundaries
+        self.poly = Polygon(boundaries)
 
     def __repr__(self):
         return 'Block #' + str(self.id) + ': ' + str(self.boundaries)
 
     #solves Point-in-Polygon problem for point (lat, lon)
-    #def contains(self, point):
+    def contains(self, point):
+        s_point = Point(point)
+        return self.poly.contains(s_point)
         
